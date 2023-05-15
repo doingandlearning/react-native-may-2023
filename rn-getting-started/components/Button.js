def94491e9fc4dc934a6f9983a2afecd884b4527
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-05-15 11:43:57 +0100

    adds data

---
+++ rn-getting-started/components/Button.js
@@ -0,0 +1,45 @@
+import {
+  View,
+  Button,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+
+export default function CustomButton() {
+  return (
+    <>
+      <Button
+        title="Click me"
+        onPress={() => console.log(`${Platform.OS} - Button was clicked!`)}
+        style={styles.button}
+      ></Button>
+      <TouchableOpacity
+        onPress={() =>
+          console.log(`${Platform.OS} - TouchableOpacity was clicked!`)
+        }
+        style={Platform.OS === "ios" ? styles.iosButton : styles.androidButton}
+      >
+        <Text>Click me instead!</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  iosButton: {
+    backgroundColor: "#ccc",
+    borderRadius: 20,
+    borderWidth: 2,
+    padding: 10,
+    margin: 2,
+  },
+  androidButton: {
+    backgroundColor: "red",
+    borderRadius: 20,
+    borderWidth: 2,
+    padding: 10,
+    margin: 2,
+  },
+});
---
+++ rn-getting-started/components/ListComponent.js
@@ -0,0 +1,11 @@
+import { FlatList } from "react-native";
+
+const DATA = [
+  { id: "1", title: "First Item" },
+  { id: "2", title: "Second Item" },
+  { id: "3", title: "Third Item" },
+];
+
+export default function ListComponent() {
+  return <></>;
+}
