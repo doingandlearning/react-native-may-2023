7a4152db83ac9098a0e22dee5afe593161bc7396
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-05-15 16:31:37 +0100

    End day 1

--- 2-rn-calling-apis/App.js
+++ 2-rn-calling-apis/App.js
@@ -4,10 +4,13 @@ import {
   Text,
   ActivityIndicator,
   SafeAreaView,
+  Button,
 } from "react-native";
 import React from "react";
 import { FlatList } from "react-native";
 import { MovieContext, MovieProvider } from "./context/MovieContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { TextInput } from "react-native";
 
 export default function WrappedApp() {
   return (
@@ -19,9 +22,37 @@ export default function WrappedApp() {
 
 function App() {
   const { movies, isLoading } = React.useContext(MovieContext);
+  const [password, setPassword] = React.useState();
+
+  React.useEffect(() => {
+    async function getAndSetPassword() {
+      const storedPassword = await AsyncStorage.getItem("rn-password");
+      console.log(storedPassword);
+
+      if (storedPassword) {
+        const parsedPassword = JSON.parse(storedPassword);
+        setPassword(parsedPassword.password);
+      }
+    }
+    getAndSetPassword();
+  }, []);
+
+  async function handleStore() {
+    try {
+      await AsyncStorage.setItem(
+        "rn-password",
+        JSON.stringify({ password: password, date: new Date() })
+      );
+    } catch (error) {
+      console.log("There was an error");
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} onChangeText={setPassword}></TextInput>
+      <Text>The password is {password}.</Text>
+      <Button title="Save password ... " onPress={handleStore}></Button>
       {isLoading ? (
         <ActivityIndicator />
       ) : movies.length === 0 ? (
@@ -55,5 +86,12 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    passing: 10,
+    width: 150,
   },
 });
