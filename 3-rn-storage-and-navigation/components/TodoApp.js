17deefef829a5df46482e4c2bc3330757c7103e4
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-05-16 10:33:44 +0100

    Adds tx to create db

--- 3-rn-storage-and-navigation/components/TodoApp.js
+++ 3-rn-storage-and-navigation/components/TodoApp.js
@@ -1,8 +1,53 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import Constants from "expo-constants";
+import React from "react";
+import * as SQLite from "expo-sqlite";
+
+// Opening db connection
+
+// Getting our todos from the db
+
+// Adding a todo to our db
+
+function openDatabase() {
+  const db = SQLite.openDatabase("TodoApp", "0.0.1");
+  return db;
+}
+
+const db = openDatabase();
 
 export default function TodoApp() {
-  return <></>;
+  const [text, setText] = React.useState("");
+
+  React.useEffect(() => {
+    // Check if the table exists:
+    // - if yes, get the todos
+    // - if no, create the table (and then get the todos)
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE table if not exists items (id integer primary key not null, done int, value text"
+      );
+    });
+  }, []);
+
+  const add = () => {};
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Howden's Todo</Text>
+      <View style={styles.flexRow}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setText}
+          value={text}
+          onSubmitEditing={() => {
+            add(text);
+            setText("");
+          }}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -27,6 +72,7 @@ const styles = StyleSheet.create({
     height: 48,
     margin: 16,
     padding: 8,
+    width: 250,
   },
   listArea: {
     backgroundColor: "#f0f0f0",
